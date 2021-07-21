c5e7237035c0acf5e743f3f4e12a01da4894b8d5
Francisco Ciccone <franciscociccone4@gmail.com>
2021-07-21 10:13:50 +0200

    Update styles on Novedades and header

--- js/novedades.js
+++ js/novedades.js
@@ -1,47 +1,3 @@
-// $.ajax({
-//     url: 'https://fakestoreapi.com/products?limit=12',
-//     dataType: "JSON",
-//     success: function(data) {
-//         renderizarProductosHTML(data);
-//     },
-// })
-
-// const productsNovedades = document.querySelector('section.products-novedades');
-
-// let list = '';
-
-// function renderizarProductosHTML() {
-
-// productsNovedades.forEach(list => {
-//     list += `
-//     <div class="col-sm-6 col-md-4 col-lg-3">
-//         <article class='card products'>
-//             <img src="${image}" class="card-img-top card-image">
-//             <div class="card-body">
-//                 <h4 class="card-title">${title}</h4>
-//                 <h5 class="card-text">${price}</h5>
-//                 <button class="btn btn-warning">Buy</button>
-//             </div>
-//         </article>
-//     </div>
-//     `;
-// });
-
-// } 
-
-// const container = $('div.');
-
-// $('button.cta-more').click(function() {
-//     $.ajax({
-//         url: '../novedades.json',
-//         method: 'GET',
-//         dataType: 'json',
-//         success: function(data) {
-//             $.each(data)
-//         },
-//     })
-// })
-
 const url = "../novedades.json";
 
 $("section.products-novedades").append('<button class="cta cta-more">Ver más</button>');
@@ -63,24 +19,3 @@ $(".cta-more").click( () => {
         }
     })
 })
-
-// const productsContainer = document.querySelector('section.products-novedades');
-
-// let list = '';
-
-// products.forEach(products => {
-//     list += `
-//     <div class="col-sm-6 col-md-4 col-lg-3">
-//         <article class='card products products-${products.id}'>
-//             <img src="${products.img}" class="card-img-top card-image" alt="${products.title}"">
-//             <div class="card-body">
-//                 <h4 class="card-title">${products.title}</h4>
-//                 <h5 class="card-text">${products.price}</h5>
-//                 <button class="btn btn-warning">Buy</button>
-//             </div>
-//         </article>
-//     </div>
-//     `;
-// });
-
-// productsContainer.innerHTML = list;
